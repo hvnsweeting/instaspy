0e75d652a5a5097a8fd768bbf86eedde09ae95fe
Viet Hung Nguyen <hvn@familug.org>
2017-08-09 23:29:20 +0700

    Lower the required elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Instaspy.Mixfile do
     [
       app: :instaspy,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       escript: escript_config(),
       start_permanent: Mix.env == :prod,
       deps: deps()
